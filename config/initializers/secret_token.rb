43e40fbf109534684c4cf7e641ae62444476139c
Brian Hays <brian.hays@gmail.com>
2013-09-18 17:08:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tumblite::Application.config.secret_key_base = '84260487699265e3be9428fbd4bf09e017d0db4ac439195ec5b4482056a087019bf28771df33953f234c3a8b1e7a9c996e6751a57c1216b9802b76c7f2504a14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tumblite::Application.config.session_store :cookie_store, key: '_tumblite_session'
