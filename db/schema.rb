59ed642ee9fbfd53306f32f42220ee37d742fed5
Brian Hays <brian.hays@gmail.com>
2013-09-21 14:19:22 -0400

    add 'kind' to post resource for text/image post options

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919155930) do
+ActiveRecord::Schema.define(version: 20130921180104) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "kind"
   end
 
 end
